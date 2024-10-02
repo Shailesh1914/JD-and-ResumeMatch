c78d7552e6967e0fe57b91310af516527b0c27fc
shailesh1914 <shailesh1914@hotmail.com>
2024-10-02 20:43:31 +0530

    Working and tested code

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#Currently the code is working for PDF file and is not working for text and doc resume files. 
 from flask import Flask, request, render_template
 import os
 import PyPDF2
 import docx2txt
 from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.feature_extraction.text import CountVectorizer # added to test, delete later
 from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
@@ -22,12 +24,12 @@ def extract_text_from_docx(file_path):
 
 def extract_text_from_txt(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        return file.read
+        return file.read()
 
 def extract_text(file_path):
     if file_path.endswith(".pdf"):
         return extract_text_from_pdf(file_path)
-    elif file_path.endswith(".doc"):
+    elif file_path.endswith(".docx"):
         return extract_text_from_docx(file_path)
     elif file_path.endswith(".txt"):
         return extract_text_from_txt(file_path)
@@ -56,23 +58,27 @@ def matcher():
         
         # Vectorize job description and resumes
         vectorizer=TfidfVectorizer().fit_transform([job_description] + resumes)
+        #vectorizer=CountVectorizer().fit_transform([job_description] + resumes) # added for testing another type of vectorizer, delete later
         vectors=vectorizer.toarray()
         
         # Cosine Similarities Calculations
         job_vector=vectors[0]
         resume_vectors=vectors[1:]
         similarities=cosine_similarity([job_vector], resume_vectors)[0]
-        #print(job_vector)
+        #similarities=cosine_similarity([job_vector], resume_vectors)[0] # for test, delete later
+        #print([resumes])
+        #print([job_vector])
         #print("==========================")
         #print(resume_vectors)
         #print("==========================")
         #print(similarities)
         
+        # Top 3 Resumes and similarity scores
         top_indices=similarities.argsort()[-3:][::-1]
         top_resumes=[resume_files[i].filename for i in top_indices]
         similarity_scores = [round(similarities[i],2) for i in top_indices]
         
-        return render_template('matchresume.html', message="Top matching resumes:", top_resumes=top_resumes, similarity_scores=similarity_scores)
+        return render_template('matchresume.html', message="Top 3 matching resumes:", top_resumes=top_resumes, similarity_scores=similarity_scores)
     return render_template('matchresume.html')
 
 if __name__=="__main__":
